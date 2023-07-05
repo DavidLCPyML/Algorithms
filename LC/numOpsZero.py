6ee6622eb1ff8e4e044011feac8d0978811754d0
DavidLCPyML <email2david2015@gmail.com>
2023-07-04 23:42:02 -0500

    7/4/23

--- LC/numOpsZero.py
+++ LC/numOpsZero.py
@@ -9,3 +9,8 @@ while num1 != 0 and num2 != 0:
     runs += 1
 
 print(runs)
+
+# 1 liner recursive
+# class Solution:
+#     def countOperations(self, num1: int, num2: int) -> int:
+#         return 1 + self.countOperations(max(num1, num2) - min(num1, num2), min(num1, num2)) if num1*num2 else 0
